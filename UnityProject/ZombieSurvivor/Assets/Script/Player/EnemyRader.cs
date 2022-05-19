69c008ab6e890dabd478eefd5d7a6b66e163d525
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-05-19 11:27:08 +0900

    first commit

---
+++ UnityProject/ZombieSurvivor/Assets/Script/Player/EnemyRader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRader : MonoBehaviour
+{
+    [SerializeField]
+    private Weapon01 weapon01;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            weapon01.target = collision.gameObject;
+        }
+        else
+        {
+            //weapon01.target = null;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            weapon01.target = null;
+        }
+
+    }
+}
---
+++ UnityProject/ZombieSurvivor/Assets/Script/Player/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerHP playerHP;
+    GameObject enemyRader;
+    
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHP = GetComponent<PlayerHP>();
+        enemyRader = GameObject.Find("EnemyRader");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //enemyRader
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {        
+        if (collision.CompareTag("Enemy"))
+        {
+            playerHP.TakeDamage(10);
+        }
+    }
+
+
+}
